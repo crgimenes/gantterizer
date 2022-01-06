092d28f3240f3ed335bae834654e4cb6719d9a19
crgimenes <crg@crg.eti.br>
2022-01-06 19:37:37 -0300

    filter default lane

--- kanbanize/main.go
+++ kanbanize/main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
+
+type task struct {
+	id           int
+	position     int
+	size         int
+	workflowname string
+	columnname   string
+	lanename     string
+}
 
 func main() {
 
@@ -49,6 +62,94 @@ func main() {
 		log.Fatalf("unable to read response body, %v", err)
 	}
 
-	fmt.Printf("%s", body)
+	m := []map[string]interface{}{}
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		log.Fatalf("unable to unmarshal response, %v", err)
+	}
+
+	tasks := []task{}
+	for _, tm := range m {
+
+		// get only cards fron the lane default
+		if tm["lanename"] != "Default" {
+			continue
+		}
+
+		// get only cards fron workflow "Tasks"
+		if tm["workflow_name"] != "Tasks" {
+			continue
+		}
+
+		// atoi taskid
+		taskid, err := strconv.Atoi(tm["taskid"].(string))
+		if err != nil {
+			log.Fatalf("unable to convert taskid to int, %v", err)
+		}
+
+		// atoi position
+		position, err := strconv.Atoi(tm["position"].(string))
+		if err != nil {
+			log.Fatalf("unable to convert position to int, %v", err)
+		}
+
+		// atoi size
+		size := 1
+		if tm["size"] != nil {
+			size, err = strconv.Atoi(tm["size"].(string))
+			if err != nil {
+				log.Fatalf("unable to convert size to int, %v", err)
+			}
+		}
+
+		// columnname
+		columnname := tm["columnname"].(string)
+
+		// ignore tasks with "Draft" in the columnname
+		if strings.Contains(columnname, "Draft") {
+			continue
+		}
+
+		// lanename
+		lanename := tm["lanename"].(string)
+
+		// workflowname
+		workflowname := tm["workflow_name"].(string)
+
+		t := task{
+			id:           taskid,
+			position:     position,
+			size:         size,
+			workflowname: workflowname,
+			columnname:   columnname,
+			lanename:     lanename,
+		}
+
+		tasks = append(tasks, t)
+		// fmt.Println(tm["taskid"], tm["workflow_name"])
+	}
+
+	columnOrder := []string{
+		"Backlog",
+		"Requested",
+		"Doing",
+		"Review",
+		"Staging",
+		"Read for Deploy",
+		"Production / Done",
+	}
+
+	// sort tasks by columnOrder and position
+	sort.Slice(tasks, func(i, j int) bool {
+		if tasks[i].columnname == tasks[j].columnname {
+			return tasks[i].position < tasks[j].position
+		}
+		return sort.SearchStrings(columnOrder, tasks[i].columnname) < sort.SearchStrings(columnOrder, tasks[j].columnname)
+	})
+
+	// List result
+	for _, t := range tasks {
+		fmt.Printf("id: %d, position: %d, size: %d, column: %s, lane: %s\n", t.id, t.position, t.size, t.columnname, t.lanename)
+	}
 
 }
